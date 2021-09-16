9f03ebf2d370ed9a230d2788ec6287aabe6e577e
jim <1150660744@qq.com>
2021-09-16 17:13:38 +0800

    add example
    Signed-off-by: jim <1150660744@qq.com>

--- main.c
+++ main.c
@@ -3,95 +3,53 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
-#include <string.h>
 #include <unistd.h>
-#include "wb_vad.h"
+#include <string.h>
+#include "vad_api.h"
 
-int main(int argc,char* argv[])   
-{      
-	if ( argc != 3 ){
-		printf("usage:./vad in-file out-file\n");
-		return -1;
+FILE *fp_all;
+
+int vad_cb(void *arg, int flag)
+{
+
+	if (fp_all != NULL) {
+		fclose(fp_all);
+		fp_all = NULL;
 	}
+	vad_log("flag: %d\n", flag);
+	return 0;
+}
 
-	char* out_file = argv[2];
+int main(int argc, char *argv[])
+{
+	int retval;
+
+	vad_str *vad;
+	vad_init(&vad, vad_cb, NULL);
+	/*1 second*/
+	vad_settime(vad, 80);
+	
 	int rc = 1;
-	char *buffer;
-	int i;
-	/*frames = 16k * 2B  * t/ 1000, t = 7.8ms*/
 	int frames = FRAME_LEN;
-
-	/*for vad*/
-	float indata[FRAME_LEN];   
-	short outdata[FRAME_LEN];   
-	VadVars *vadstate;                     
-	char name[128] = {0}; 
-	int temp, vad;
-	int recording = 0;
-	int count = 0;
-	FILE *fp = NULL;
-	FILE *fp_all = NULL;
-	/* two bytes*/
-	buffer = (char*)malloc(frames * 2);
-
-	/*vad init*/
-	wb_vad_init(&(vadstate));
-	sprintf(name, "%s.pcm", out_file);
-	fp_all = fopen(name, "wb");
-
 	int in_fd = open(argv[1], O_RDONLY, 0);
-	/*wav header 44 byte*/
-	lseek(in_fd, 4, SEEK_SET);
-	int len = 0;
+	char *buffer = (char*)malloc(frames * 2);
+
+	fp_all = fopen("second", "wb");
 	while (rc > 0) {
 		rc = read(in_fd, buffer, frames);
-
-		for(i = 0; i< frames; i++) {
-			indata[i]=0;
-			temp = 0;
-			memcpy(&temp, buffer + 2 * i, 2);
-			indata[i]=(short)temp;
-			outdata[i]=temp;
-			if (indata[i] > 65535/2)
-				indata[i] = indata[i]-65536;
+		retval = vad_feed(vad, buffer, frames);
+		if (retval == FAIL) {
+			vad_log("vad feed fail\n");
+			goto out;
 		}
-		vad = wb_vad(vadstate, indata);
-		/*check the vad flag, 1: speech, other: noise */
-		if(vad == 1) {
-			len = 0;
-			recording = 1;
-		} else {
-			recording = 0;
-			/*1 second delay*/
-			if (len < 80) {
-				len++;
-				recording = 1;
-				goto out;
-			} else {
-				printf("detect the slient\n");
-				len = 0;
-			}
-		}
-
-		if (recording == 1 && fp == NULL) {
-			sprintf(name, "%s.%d.pcm", out_file, count);
-			fp=fopen(name, "wb");
-		}
-
-		if (recording == 0 && fp != NULL) {
-			fclose(fp);
-			fp = NULL;
-			count++;
-		}
-out:		if (fp != NULL && recording == 1)
-			fwrite(outdata, 1, FRAME_LEN, fp);
 		if (fp_all != NULL)
-			fwrite(outdata, 1, FRAME_LEN, fp_all);
+			fwrite(buffer, 1, frames, fp_all);
 	}
-	wb_vad_exit(&vadstate);
 	free(buffer);
+	if (fp_all != NULL)
+		fclose(fp_all);
 	close(in_fd);
-	//fclose(fp_all);
-	//fclose(fp);
-	fcloseall();
+	vad_delete(&vad);
+out:
+	return 0;
 }
