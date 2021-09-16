3e66864f6ba0327642c046ed6582e4666fd38054
jim <1150660744@qq.com>
2021-09-16 17:34:47 +0800

    add note
    Signed-off-by: jim <1150660744@qq.com>

--- vad_api.h
+++ vad_api.h
@@ -17,7 +17,13 @@
 
 typedef VadVars vad_str;
 
+/*When silent it will call the cb function to notify user*/
 int vad_init(vad_str **entity, VAD_CB cb, void *arg);
+
+/*set vad detect time*/
 int vad_settime(vad_str *entity, float ms);
+
+/*audio input*/
 int vad_feed(vad_str *entity, char *data, int len);
+
 int vad_delete(vad_str **entity);
